cec74b86f84ce9914260125880e66689e2ec1109
hamza lam <155950378+7amzalam@users.noreply.github.com>
2025-07-26 05:39:50 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// js/script.js
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'deep-blue': '#0a2540',
+                'gold': '#c8a97e',
+                'beige': '#f5f1e9',
+                'sand': '#e8e0d3',
+            },
+            fontFamily: {
+                'serif': ['Playfair Display', 'serif'],
+                'sans': ['Montserrat', 'sans-serif'],
+            }
+        }
+    }
+}
